d407fa36ce13ba3f5e3b7fe60e38dae1cc0591f9
lokeshkonka <konkalokesh372@gmail.com>
2026-06-19 22:12:40 +0530

    Docker Changes and Configs

--- backend/Configs/env.ts
+++ backend/Configs/env.ts
@@ -4,4 +4,5 @@ export const env = {
   PORT: Number(process.env.PORT) || 3000,
   DATABASE_URL: process.env.DATABASE_URL!,
   NODE_ENV: process.env.NODE_ENV || "development",
+  REDIS_URL: process.env.REDIS_URL!,
 };
