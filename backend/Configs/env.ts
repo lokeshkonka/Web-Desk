82ec55d187b53f5ec9d7097ccff49d6907b08da6
lokeshkonka <konkalokesh372@gmail.com>
2026-06-21 13:33:12 +0530

    update environment settings

--- backend/Configs/env.ts
+++ backend/Configs/env.ts
@@ -5,4 +5,10 @@ export const env = {
   DATABASE_URL: process.env.DATABASE_URL!,
   NODE_ENV: process.env.NODE_ENV || "development",
   REDIS_URL: process.env.REDIS_URL!,
+  MINIO_ENDPOINT: process.env.MINIO_ENDPOINT,
+  MINIO_PORT: process.env.MINIO_PORT,
+  MINIO_USE_SSL: process.env.MINIO_USE_SSL,
+  MINIO_ACCESS_KEY: process.env.MINIO_ACCESS_KEY,
+  MINIO_SECRET_KEY: process.env.MINIO_SECRET_KEY,
+  MINIO_BUCKET: process.env.MINIO_BUCKET,
 };
